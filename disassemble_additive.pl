60e251aec19516074d5cbd45c0d47f1b89aec492
Joe Marsh <jmarsh@Joe-Marsh-MBP.local>
2015-12-09 14:00:47 +0000

    faster

--- disassemble_additive.pl
+++ disassemble_additive.pl
@@ -12,6 +12,9 @@ while(<I>){
 	next unless ($c[2] > 1);
 	next unless ($all_on or ($chain_on{$c[0]} and $chain_on{$c[1]}));
 	push @interfaces, "$c[0] $c[1] $c[2]\n";
+	$isize{"$c[0] $c[1]"} = $c[2];
+	$isize{"$c[1] $c[0]"} = $c[2];
+	$ni++;
 	$all{$c[0]}=1;
 	$all{$c[1]}=1;
 }
@@ -26,7 +29,7 @@ while(@subs){
 	%on=%ixn=%n=();
 	@is=();
 
-	$on{$_} = 1 for (split '\-', $sub);
+	$on{$_} = 1 for (split '-', $sub);
 
 	for(@interfaces){
 		@c=split;
@@ -37,12 +40,10 @@ while(@subs){
 	}
 
 	$best=$bestx=$rest="";
-	for $l (1..(int(slength($sub)/2))){
-		%done=();
-		for $ca (sort(keys %on)){
-			nsi($ca, $l);
-		}
-		
+	%done=();
+	#try all possible connected subcomplexes, up to half the size of the complex
+	for $ca (sort(keys %on)){
+		nsi($ca, int(slength($sub)/2));
 	}
 
 	for(sort(keys %on)){
@@ -64,51 +65,52 @@ while(@subs){
 
 sub nsi { #recursion!
 	my ($x, $nl)=@_;
-	$x = sortc($x);
-	return "" if ($done{$x});
 	$done{$x}=1;
-	return "" if (repeats($x));
-	if (slength($x)==$nl){ 
-		$ix = checki($x);
-		if ($ix < $best or $best eq ''){
-			$best = $ix;
-			$bestx = $x;
-		}
-	}else{
-		%duni=();
-		for $cx (split '\-', $x){
+	$ix = checki($x);
+	if ($ix < $best or $best eq ''){
+		$best = $ix;
+		$bestx = $x;
+	}
+	if (slength($x) < $nl){
+		my %duni=();
+		$duni{$_} = 1 for (split '-', $x);
+		for $cx (split '-', $x){
+			$max=0;
 			for (1..$n{$cx}){
 				$i = $ixn{$cx}[$_];
 				next if ($duni{$i});
-				$duni{$cx}=1;
-				nsi("$x-$i", $nl);
+				$duni{$i}=1;
+				$inew=0;
+				for $cx (split '-', $x){
+					$inew += $isize{"$i $cx"};
+				}
+				if ($inew > $max){ #add the subunit that forms the most interface with the subcomplex
+					$max = $inew;
+					$addi = $i;
+				}
 			}
+			$y = sortc("$x-$addi");
+			next if ($done{$y});
+			nsi($y, $nl);
 		}
 	}
 }
 
 sub sortc{
 	my @chars;
-	for $cx (split '\-', $_[0]){
+	for $cx (split '-', $_[0]){
 		push @chars, $cx;
 	}
 	return (join "-", sort(@chars));
 }
-sub repeats{
-	my %charon;
-	for $cx (split '\-', $_[0]){
-		$charon{$cx}++;
-		return 1 if ($charon{$cx} > 1);
-	}
-	return 0;
-}
+
 sub checki{
 	$ii=0;
 	%son=();
-	$son{$_} = 1 for (split '\-', $_[0]);
+	$son{$_} = 1 for (split '-', $_[0]);
 	for(@is){
 		($a,$b,$i) = split;
-		if (($son{$a}==1 and $son{$b}!=1) or ($son{$a}!=1 and $son{$b}==1)){
+		if (($son{$a}==1 and $son{$b}!=1) or ($son{$a}!=1 and $son{$b}==1)){ #check for broken interfaces
 			$ii += $i;
 		}
 	}
@@ -117,6 +119,6 @@ sub checki{
 
 sub slength{
 	my $sln=0;
-	$sln++ for (split '\-', $_[0]);
+	$sln++ for (split '-', $_[0]);
 	return $sln;
 }
